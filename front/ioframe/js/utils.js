8cdc9c4d7d411815a40a98677cb6b6be4dad0f27
IgalOgonov <igal1333@hotmail.com>
2019-06-22 18:22:28 +0300

    Added a missing function.

--- front/ioframe/js/utils.js
+++ front/ioframe/js/utils.js
@@ -62,6 +62,57 @@ function generateFP(attrName, target){
     });
 }
 
+// Simple but unreliable function to create string hash by Sergey.Shuchkin [t] gmail.com
+// alert( strhash('http://www.w3schools.com/js/default.asp') ); // 6mn6tf7st333r2q4o134o58888888888
+function strhash( str ) {
+    if (str.length % 32 > 0) str += Array(33 - str.length % 32).join("z");
+    var hash = '', bytes = [], i = j = k = a = 0, dict = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','1','2','3','4','5','6','7','8','9'];
+    for (i = 0; i < str.length; i++ ) {
+        ch = str.charCodeAt(i);
+        bytes[j++] = (ch < 127) ? ch & 0xFF : 127;
+    }
+    var chunk_len = Math.ceil(bytes.length / 32);   
+    for (i=0; i<bytes.length; i++) {
+        j += bytes[i];
+        k++;
+        if ((k == chunk_len) || (i == bytes.length-1)) {
+            a = Math.floor( j / k );
+            if (a < 32)
+                hash += '0';
+            else if (a > 126)
+                hash += 'z';
+            else
+                hash += dict[  Math.floor( (a-32) / 2.76) ];
+            j = k = 0;
+        }
+    }
+    return hash;
+}
+
+// Returns a function, that, as long as it continues to be invoked, will not
+// be triggered. The function will be called after it stops being called for
+// N milliseconds.
+// This function generates a global scope timer for the function.
+// The timer is either based on the hash of the function source, or is given as input.
+function debounce(func, wait, timerName = '') {
+    if(timerName == '')
+        timerName = strhash(func.toString());
+    if(document[timerName] === undefined)
+        document[timerName] = {
+            'function': function(){},
+            'lastCalled':Date.now()
+        };
+	return function() {
+        
+		var context = this, args = arguments;
+		var later = function() {
+			document[timerName].function = function(){};
+			func.apply(context, args);
+		};
+		clearTimeout(document[timerName].function);
+		document[timerName].function = setTimeout(later, wait);
+	};
+};
 
 //Checks if we are logged in, returns resault using the dfd because it might need to call the server
 function checkLoggedIn(pathToRoot,trustLocalStorage=false){
